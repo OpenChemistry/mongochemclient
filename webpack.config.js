07b0a7a904a452024b86a26925aba422bf4ecb02
Chris Harris <chris.harris@kitware.com>
2015-05-06 11:41:59 -0400

    Fix baggage loading of templates

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,43 @@
 var webpack = require('webpack');
 var path = require('path')
 var glob = require('glob');
-var ngAnnotatePlugin = require('ng-annotate-webpack-plugin')
-
-// This is required to init the baggage-loader
-require('baggage-loader')
+var ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
+var fs = require('fs');
 
 appRoot = path.join(__dirname, 'app');
+
+/**
+ * Function to extract out any file paths referenced by requires(...)
+ *
+ */
+function extractRoutes() {
+    var lines = fs.readFileSync(path.join(appRoot, 'app.routes.js')).toString().split("\n");
+
+    var requires = lines.map(function(item) {
+        var match = item.match(/^\s*templateUrl:\s*require\(['"](.*)['"]\).*$/);
+        if (match) {
+            return match[1];
+        }
+    });
+
+    return requires;
+}
+
+/**
+ * Function to generate the appropriate query parameter to pass to baggage loader
+ */
+function generateBaggageQueryRoutes() {
+    var paths = extractRoutes()
+
+    return paths.join('&');
+}
 
 module.exports = {
     entry: glob.sync(appRoot + '/**/*.js'),
     resolve: {
-        alias: {style: path.join(__dirname, 'assets/style')}
+        alias: {
+            style: path.join(__dirname, 'assets/style')
+        }
     },
     devtool: 'sourcemap',
     output: {
@@ -21,7 +47,10 @@ module.exports = {
     module: {
         preLoaders: [
             { test: /3Dmol.js$/, loader: '3dmol-patch'},
+            { test: /directive\.js$/, loader: 'baggage?[dir].view.html' },
+            { test: /directive\.js$/, loader: 'baggage?[dir].view.jade' },
             { test: /\.js$/, loader: 'baggage?[file].html' },
+            { test: /app\.routes\.js$/, loader: 'baggage?' + generateBaggageQueryRoutes()},
             { test: /\.js$/,
               exclude: /node_modules/,
               loader: "jshint-loader"
@@ -30,8 +59,8 @@ module.exports = {
         loaders: [
             { test: /\.css$/, loader: 'style!css' },
             { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
-            { test: /\.html$/, loader: 'ngtemplate?relativeTo=' + __dirname + '/!html' },
-            { test: /\.jade$/, loader: 'ngtemplate?relativeTo=' + __dirname + '/!html!jade-html-loader' },
+            { test: /\.html$/, loader: 'ngtemplate?relativeTo=' + __dirname + '!html' },
+            { test: /\.jade$/, loader: 'ngtemplate?relativeTo=' + __dirname + '!html!jade-html-loader' },
             { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader' }
         ]
     },

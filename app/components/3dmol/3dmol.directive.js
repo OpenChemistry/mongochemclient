15a9d47839592689a848150193cb3bff9dd74c79
Chris Harris <chris.harris@kitware.com>
2015-05-07 11:18:44 -0400

    Ensure 3Dmol view is full width
    We need to delay the render to the next call of the digest loop.
    So that the element size is correct.

--- app/components/3dmol/3dmol.directive.js
+++ app/components/3dmol/3dmol.directive.js
@@ -1,30 +1,33 @@
 require('script!3Dmol/release/3Dmol.js');
-require('style/3dmol.styl');
 
 angular.module('chemPhyWebApp')
-    .directive('chemPhyWeb3dmol', ['$http', '$log', function($http, $log) {
+    .directive('chemPhyWeb3dmol', ['$http', '$log', '$timeout', function($http, $log, $timeout) {
 
         return {
             scope: {},
-            link: function($scope, $element) {
-                // Viewer config - properties 'defaultcolors' and 'callback'
-                var config = {defaultcolors: $3Dmol.rasmolElementColors };
+            link: function postLink($scope, $element) {
 
-                // Create GLViewer within 'gldiv'
-                $scope.viewer = $3Dmol.createViewer($($element), config);
-                $scope.viewer.setBackgroundColor(0xeeeeee);
-                $scope.viewer.resize();
+                // Run in next digest so we get the right element size
+                $timeout(function() {
+                    // Viewer config - properties 'defaultcolors' and 'callback'
+                    var config = {defaultcolors: $3Dmol.rasmolElementColors };
 
-                $http.get('/data/2POR.pdb')
-                    .success(function(data) {
-                        $scope.viewer.addModel(data, 'pdb');
-                        $scope.viewer.setStyle({}, {stick:{}});
-                        $scope.viewer.zoomTo();
-                        $scope.viewer.render();
-                    })
-                    .error(function(data) {
-                        $log.error(data);
-                    });
+                    // Create GLViewer within 'gldiv'
+                    $scope.viewer = $3Dmol.createViewer($($element), config);
+                    $scope.viewer.setBackgroundColor(0xeeeeee);
+                    $scope.viewer.resize();
+
+                    $http.get('/data/2POR.pdb')
+                        .success(function(data) {
+                            $scope.viewer.addModel(data, 'pdb');
+                            $scope.viewer.setStyle({}, {stick:{}});
+                            $scope.viewer.zoomTo();
+                            $scope.viewer.render();
+                        })
+                        .error(function(data) {
+                            $log.error(data);
+                        });
+                });
             }
         };
     }]);

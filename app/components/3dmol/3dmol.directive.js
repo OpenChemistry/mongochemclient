a277e1fb1b31f357b539707404274513643ab107
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2015-11-16 13:00:33 -0500

    Correct bug in MO dialog code - start from 1

--- app/components/3dmol/3dmol.directive.js
+++ app/components/3dmol/3dmol.directive.js
@@ -291,7 +291,7 @@ require.ensure(['script!3Dmol/build/3Dmol.js'], function(require) {
                         $scope.orbitals = {};
                         $scope.orbitals.electronCount = $scope.cjson.basisSet.electronCount;
                         $scope.orbitals.mos = [];
-                        for (let i = 0; i < $scope.orbitals.electronCount; ++i) {
+                        for (let i = 1; i <= $scope.orbitals.electronCount; ++i) {
                             let text = '';
                             if (i == $scope.orbitals.electronCount / 2) {
                                 text = ' (HOMO)';

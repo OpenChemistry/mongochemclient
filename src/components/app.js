2ecc74a82e175349daeac1fe273564e2efeda1b8
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2017-07-07 13:57:55 -0400

    Minor rebranding

--- src/components/app.js
+++ src/components/app.js
@@ -23,7 +23,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to MongoChemWeb</h2>
+          <h2>Welcome to Open Chemistry on the web!</h2>
         </div>
         <pre>
           { JSON.stringify(this.props.molecules, null, 2) }

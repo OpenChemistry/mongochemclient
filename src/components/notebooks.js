e84fa530909f9ffcc6f6768b1cc7a1d138a2c762
Alessandro Genova <alessandro.genova@kitware.com>
2018-06-29 14:33:21 -0400

    Port notebooks to material-ui v1.x
    NOTE: Untested

--- src/components/notebooks.js
+++ src/components/notebooks.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types';
-import {
-  Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-  TableRowColumn,
-} from 'material-ui/Table';
-import Avatar from 'material-ui/Avatar';
-import InsertDriveFile from 'material-ui/svg-icons/editor/insert-drive-file';
-import {blue500} from 'material-ui/styles/colors';
+
+import Avatar from '@material-ui/core/Avatar';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+import InsertDriveFile from '@material-ui/icons/InsertDriveFile';
+
+import blue from '@material-ui/core/colors/blue';
+
 import filesize from 'filesize'
 import moment from 'moment'
 import _ from 'lodash';
 
-
 import { redirectToJupyterHub } from '../redux/ducks/jupyterlab'
+
+const blue500 = blue['500'];
 
 const style = {
   iconColumn:  {
-    width: '20px'
+    width: 20
   }
 }
 
@@ -77,39 +79,36 @@ class Notebooks extends Component {
 
     return (
         <div>
-        <Table
-          onCellClick={this.onCellClick}
-        >
-          <TableHeader
-            fixedHeader={true}
-            displaySelectAll={false}
-            adjustForCheckbox={false}
-          >
-            <TableRow>
-              <TableHeaderColumn style={style.iconColumn}></TableHeaderColumn>
-              <TableHeaderColumn></TableHeaderColumn>
-              <TableHeaderColumn>Last Modified</TableHeaderColumn>
-              <TableHeaderColumn>Size</TableHeaderColumn>
-            </TableRow>
-          </TableHeader>
-          <TableBody
-            displayRowCheckbox={false}
-            showRowHover={true}
-          >
-          {notebooks.map((notebook, i) =>
-            <TableRow
-              key={notebook._id}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell style={style.iconColumn}></TableCell>
+                <TableCell></TableCell>
+                <TableCell>Last Modified</TableCell>
+                <TableCell>Size</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody
+              displayRowCheckbox={false}
+              showRowHover={true}
             >
-              <TableRowColumn style={style.iconColumn}>
-                <Avatar icon={<InsertDriveFile />} backgroundColor={blue500} />
-              </TableRowColumn>
-              <TableRowColumn>{notebook.name}</TableRowColumn>
-              <TableRowColumn>{moment(notebook.created).fromNow()}</TableRowColumn>
-              <TableRowColumn>{filesize(notebook.size)}</TableRowColumn>
-            </TableRow>
-          )}
-          </TableBody>
-        </Table>
+            {notebooks.map((notebook, i) =>
+              <TableRow
+                onClick={event => this.onCellClick(i)}
+                key={notebook._id}>
+              >
+                <TableCell style={style.iconColumn}>
+                  <Avatar style={{backgroundColor: blue500}}>
+                    <InsertDriveFile />
+                  </Avatar>
+                </TableCell>
+                <TableCell>{notebook.name}</TableCell>
+                <TableCell>{moment(notebook.created).fromNow()}</TableCell>
+                <TableCell>{filesize(notebook.size)}</TableCell>
+              </TableRow>
+            )}
+            </TableBody>
+          </Table>
         </div>
     );
   }

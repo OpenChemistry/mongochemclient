5b94c9d3af957d6af5e5e9a93cdd0b6be453f806
Alessandro Genova <alessandro.genova@kitware.com>
2019-03-29 09:47:07 -0400

    Create a new card component to show calculation/molecule details

---
+++ src/utils/strings.js
@@ -0,0 +1,43 @@
+export function isUpperCase(c) {
+  return c === c.toUpperCase();
+}
+
+export function isLowerCase(c) {
+  return c === c.toLowerCase();
+}
+
+export function capitalizeFirst(s) {
+  return s.charAt(0).toUpperCase() + s .slice(1);
+}
+
+export function toUpperCase(s) {
+  return String.prototype.toUpperCase.call(s);
+}
+
+export function toLowerCase(s) {
+  return String.prototype.toLowerCase.call(s);
+}
+
+export function camelToSpace(s) {
+  if (s.length < 2) {
+    return s;
+  }
+
+  let wordsIdx = [0];
+
+  for (let i = 1; i < s.length - 1; ++i) {
+    if (isUpperCase(s.charAt(i)) && (isLowerCase(s.charAt(i - 1)) || isLowerCase(s.charAt(i + 1)))) {
+      wordsIdx.push(i);
+    }
+  }
+
+  let words = [];
+  for (let i = 0; i < wordsIdx.length - 1; ++i) {
+    words.push(s.slice(wordsIdx[i], wordsIdx[i + 1]));
+  }
+  words.push(s.slice(wordsIdx[wordsIdx.length - 1]));
+
+  words = words.map(w => capitalizeFirst(w));
+
+  return words.join(' ');
+}

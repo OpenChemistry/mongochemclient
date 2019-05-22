368fb8c2642321829504fc8c45adbfd2968e5636
Patrick Avery <patrick.avery@kitware.com>
2019-05-22 08:59:31 -0400

    Remove selectEmpty style, as it is not used
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- src/components/molecules.js
+++ src/components/molecules.js
@@ -26,10 +26,7 @@ const styles = theme => ({
   formControl: {
     margin: theme.spacing.unit,
     minWidth: 120,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing.unit * 2,
-  },
+  }
 });
 
 class Molecules extends Component {

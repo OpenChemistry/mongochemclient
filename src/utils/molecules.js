de42d6f3c789c0a1edab8de8b1cb3c87f1a617f4
Patrick Avery <patrick.avery@kitware.com>
2019-06-12 11:30:06 -0400

    Simplify logic for has3dCoords
    This just results in less code to do the exact same thing...
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- src/utils/molecules.js
+++ src/utils/molecules.js
@@ -2,8 +2,5 @@ import { get } from 'lodash-es';
 
 export function has3dCoords(molecule) {
   const coords = get(molecule, 'cjson.atoms.coords.3d')
-  if (coords === undefined || coords.length == 0)
-    return false
-
-  return true
+  return coords !== undefined && coords.length > 0
 }

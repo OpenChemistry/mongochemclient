0e3556996799ff65287d6969a030a2326fc32e56
Brianna Major <brianna.major@kitware.com>
2019-11-14 14:41:36 -0500

    Move Creator module to client
    This component is specific to Open Chemistry and belongs here rather than with
    the Girder UI components.
    
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- src/containers/user/creator-profile.js
+++ src/containers/user/creator-profile.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import CreatorProfile from '../../components/user/creator-profile';
 
+import { calculations, molecules, selectors } from '@openchemistry/redux';
+
 class CreatorContainer extends Component {
+
+  componentDidMount() {
+    const { location, dispatch } = this.props;
+    const { type, id } = location.state;
+    if (type === 'calculation') {
+      dispatch(calculations.loadCalculationById(id))
+    } else {
+      dispatch(molecules.loadMoleculeById(id))
+    }
+  }
+
   render() {
-    console.log('CreatorContainer - Client')
     return (
-      <CreatorProfile {...this.props}/>
-    )
+      <CreatorProfile
+        {...this.props}
+      />
+    );
   }
 }
+
+function mapStateToProps(state) {
+  const userInfo = selectors.calculations.getCalculationCreator(state);
+  let twitterId = userInfo.twitterId;
+  let orcidId = userInfo.orcidId;
+
+  if (!twitterId) {
+    twitterId = undefined
+  }
+  if (!orcidId) {
+    orcidId = undefined
+  }
+  const mediaIds = {twitterId, orcidId}
+
+  return {
+    userInfo,
+    mediaIds
+  };
+}
+
+CreatorContainer = connect(mapStateToProps)(CreatorContainer)
 
 export default CreatorContainer;

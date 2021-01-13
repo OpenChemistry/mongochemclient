5faa5c16589b9e76a383b61a699f6154bd545dea
Alessandro Genova <alessandro.genova@kitware.com>
2021-01-13 16:06:51 -0500

    Add a few missing peer dependencies to resolve.alias

--- config-overrides.js
+++ config-overrides.js
@@ -3,8 +3,13 @@ module.exports = {
   webpack: (config, env) => {
     if (env === 'development') {
       config.resolve.alias = {
-        react: path.resolve('node_modules/react'),
+        'react': path.resolve('node_modules/react'),
+        'react-dom': path.resolve('node_modules/react-dom'),
         'react-redux': path.resolve('node_modules/react-redux'),
+        'redux': path.resolve('node_modules/redux'),
+        'redux-saga': path.resolve('node_modules/redux-saga'),
+        'redux-form': path.resolve('node_modules/redux-form'),
+        '@material-ui': path.resolve('node_modules/@material-ui'),
         '@openchemistry': path.resolve('node_modules/@openchemistry')
       };
     }
